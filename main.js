a06ef6e2d2dba0ad557df790db13dcd9dd0ea939
Tuxu <t.u.x.u@live.com>
2020-04-12 22:50:45 -0300

    add meta tages and fixed bugs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
         var teste = data.filter(element => element.status == "today")
-        var template = Handlebars.compile($('#item-template').html())
+        var template = Handlebars.compile($('#lives').html())
         $('#list').html(template(teste))
     });
 });
@@ -10,7 +10,7 @@ function todayLives() {
     $('#list').fadeOut();
     $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
         var today = data.filter(element => element.status == "today")
-        var template = Handlebars.compile($('#item-template').html())
+        var template = Handlebars.compile($('#lives').html())
         $('#list').html(template(today)).fadeIn();
     });
 }
@@ -18,7 +18,7 @@ function todayLives() {
 function allLives() {
     $('#list').fadeOut();
     $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
-        var template = Handlebars.compile($('#item-template').html())
+        var template = Handlebars.compile($('#lives').html())
         $('#list').html(template(data));
         $('#list').fadeIn();
     });
