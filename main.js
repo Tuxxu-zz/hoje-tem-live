d3e350b407de6907612315da5f8a2c9cb054e3ec
Tuxu <t.u.x.u@live.com>
2020-04-12 22:42:33 -0300

    add: meta tags and fixed bugs

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-$(document).ready(function(){
-$.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
-		var template = Handlebars.compile($('#item-template').html())
-		$('#list').html(template(data))
-	})
-})
+$(document).ready(function() {
+    $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
+        var teste = data.filter(element => element.status == "today")
+        var template = Handlebars.compile($('#lives').html())
+        $('#list').html(template(teste))
+    });
+});
+
+function todayLives() {
+    $('#list').fadeOut();
+    $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
+        var today = data.filter(element => element.status == "today")
+        var template = Handlebars.compile($('#lives').html())
+        $('#list').html(template(today)).fadeIn();
+    });
+}
+
+function allLives() {
+    $('#list').fadeOut();
+    $.getJSON('https://sheet.best/api/sheets/2f8d9274-9901-47fa-a582-01d445d4c709', function(data) {
+        var template = Handlebars.compile($('#lives').html())
+        $('#list').html(template(data));
+        $('#list').fadeIn();
+    });
+}
